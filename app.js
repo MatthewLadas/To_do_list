d7f0dc711daf052124d5cb6f33b35dc66f1216c0
MatthewLadas <matthew.ladas@outlook.com>
2021-05-15 12:41:08 -0400

    Added in a delete feature

--- app.js
+++ app.js
@@ -14,8 +14,13 @@ while (input !== 'quit' && input !== 'q') {
         todos.push(newTodo);
         console.log(`Awesome! ${newTodo} has been added to the list.` );
     } else if (input === 'delete') {
-        const index = prompt('Ok, enter an index to delete that entry.');
-        
+        const index = parseInt(prompt('Ok, enter an index to delete that entry.'));
+        if (!Number.isNaN(index)) {
+            const deleted = todos.splice(index, 1);
+            console.log(`Great! ${deleted[0]} has been deleted!`);
+        } else {
+            console.log('Unkown Index');
+        }
     }
     input = prompt("What would you like to do?");
 }
